onemodel/onemodel

commit 8ea87730e59e27f9af9add5fc9b1da8e5f8d3363

Expand & improve support for auto-adding default attributes per class.

src/main/scala/org/onemodel/TextUI.scala
@@ -212,24 +212,24 @@ class TextUI(args: Array[String] = Array[String](), val inIn: Option[InputStream
     controller.start()
   }
 
-  /* Returns the string entered (None if the user just wants out of this question or whatever, OR the default value in that case if one is provided. Is that
-     true, or does it  always currently return just an empty string? Looks so. See internal comments for possibly fixing that.)
+  /* Returns the string entered (None if the user just wants out of this question or whatever, unless useDefaultIfBlankedIn is true).
    * The parameter "criteria"'s Option is a function which takes a String (which will be the user input), which it checks for validity.
    * If the entry didn't meet the criteria, it repeats the question until it does or user gets out w/ ESC.
    * A simple way to let the user know why it didn't meet the criteria is to put them in the leading text.
    */
   //@tailrec //see below note on 'recursive' for why removed 4 now.
-  final def askForString(inLeadingText: Option[Array[String]],
-                         inCriteria: Option[(String) => Boolean] = None,
+  final def askForString(leadingTextIn: Option[Array[String]],
+                         criteriaIn: Option[(String) => Boolean] = None,
                          inDefaultValue: Option[String] = None,
-                         inIsPassword: Boolean = false): Option[String] = {
+                         isPasswordIn: Boolean = false,
+                         useDefaultIfBlankedIn: Boolean = false): Option[String] = {
     var count = 0
     val lastLineOfPrompt: String = {
       var lastLineOfPrompt = ""
-      if (inLeadingText.isDefined) {
-        for (prompt <- inLeadingText.get) {
+      if (leadingTextIn.isDefined) {
+        for (prompt <- leadingTextIn.get) {
           count = count + 1
-          if (count < inLeadingText.get.length) {
+          if (count < leadingTextIn.get.length) {
             // all but the last one
             println(prompt)
           }
@@ -270,32 +270,27 @@ class TextUI(args: Array[String] = Array[String](), val inIn: Option[InputStream
     }.start()
 
     //jlineReader.readLine()
-    val line = jlineReader.readLine(null, if (inIsPassword) '*' else null)
+    val line = jlineReader.readLine(null, if (isPasswordIn) '*' else null)
     if (line == null) {
       None
     }
     else {
       def checkCriteria(line: String): Option[String] = {
-        if (inCriteria.isEmpty || inCriteria.get(line)) {
+        if (criteriaIn.isEmpty || criteriaIn.get(line)) {
           Some(line)
         }
         else {
           displayText("Didn't pass the criteria; please re-enter.")
           // this gets "recursive call not in tail position", until new version of jvm that allows scala2do it?
-          askForString(inLeadingText, inCriteria, inDefaultValue, inIsPassword)
+          askForString(leadingTextIn, criteriaIn, inDefaultValue, isPasswordIn)
         }
       }
 
-      if (line.length() == 0 && inDefaultValue.isDefined) {
-        // idea: we are currently taking the default value even if there are criteria and it fails: That could be reconsidered.
-        // If this is changed, places calling it that rely on its  behavior should be re-evaluated (ie, what is the behavior & how should callers use that?
-        // Especially for Controller.askForAttributeValidAndObservedDates, which currently, unfortunately, relies on this mis-behavior, and change the method
-        // comment describing its contract TO CORRECTLY DESCRIBE THE MEANING & OPERATION!
-        // Maybe the callers should control it via whether there is a default value, & change the cmt of this method, or, they should act based on whether the
-        // returned info is blank.  Does this mean also that we should add tests for the text UI, now?
+      if (line.isEmpty && inDefaultValue.isDefined && useDefaultIfBlankedIn) {
         checkCriteria(inDefaultValue.get)
-      }
-      else {
+      } else if (line.isEmpty) {
+        None
+      } else {
         checkCriteria(line)
       }
     }

src/main/scala/org/onemodel/controller/ClassMenu.scala
@@ -24,7 +24,7 @@ class ClassMenu(val ui: TextUI, db: PostgreSQLDatabase, controller: Controller) 
       val leadingText: Array[String] = Array[String]("CLASS: " + classIn.getDisplayString)
       val choices = Array[String]("(stub: classes can be added while creating a new entity)" /*"add"' option, if needed*/ ,
                                   "(stub)" /*"sort" if needed*/ ,
-                                  "Edit class name",
+                                  "Edit...",
                                   "Delete",
                                   "Go to defining entity",
                                   "Search (List all entities in this class)")
@@ -33,8 +33,37 @@ class ClassMenu(val ui: TextUI, db: PostgreSQLDatabase, controller: Controller) 
       else {
         val answer = response.get
         if (answer == 3) {
-          controller.askForAndWriteClassAndDefiningEntityName(Some(classIn.getId), Some(classIn.getName))
-          classMenu(new EntityClass(db, classIn.getId))
+          val currentCreateDefaultAttrValue: Option[Boolean] = classIn.getCreateDefaultAttributes
+          val asDisplayed = {
+            if (currentCreateDefaultAttrValue.isEmpty) "unset"
+            else if (currentCreateDefaultAttrValue.get) "true" else "false"
+          }
+          val editResponse = ui.askWhich(None, Array[String]("Edit class name",
+                                                             "Edit \"Create template attributes by default on new entities\" value (currently " + asDisplayed + ")"))
+          if (editResponse.isEmpty) None
+          else if (editResponse.get == 1) {
+            controller.askForAndWriteClassAndDefiningEntityName(Some(classIn.getId), Some(classIn.getName))
+            classMenu(new EntityClass(db, classIn.getId))
+          } else if (editResponse.get == 2) {
+            val prompt = "Do you want the program to create all the attributes by default, when creating a new entity in this class, using " +
+                         "the class defining entity's attributes as a template?  Enter a yes/no value (or a space for 'unknown/unspecified', i.e., to " +
+                         "ask every time)"
+            val valueBefore: Option[Boolean] = db.getClassCreateDefaultAttributes(classIn.getId)
+            val defaultValue: String = valueBefore match {
+              case Some(true) => "y"
+              case Some(false) => "n"
+              case None => " "
+            }
+            val valueEntered: Option[Boolean] = ui.askYesNoQuestion(prompt, Some(defaultValue), allowBlankAnswer = true)
+            if (valueBefore != valueEntered) {
+              db.updateClassCreateDefaultAttributes(classIn.getId, valueEntered)
+            }
+            classMenu(new EntityClass(db, classIn.getId))
+          } else {
+            //textui doesn't actually let the code get here, but:
+            ui.displayText("invalid response")
+            None
+          }
         }
         else if (answer == 4) {
           val entitiesCount: Long = db.getEntitiesOnlyCount(Some(classIn.getId), limitByClass = true, Some(classIn.getDefiningEntityId))

src/main/scala/org/onemodel/controller/EntityMenu.scala
@@ -159,6 +159,7 @@ class EntityMenu(override val ui: TextUI, override val db: PostgreSQLDatabase, v
                                                                      if (highlightedEntry.isDefined) Some(highlightedEntry.get.getId) else None,
                                                                      numDisplayableAttributes, newAttribute.getFormId,
                                                                      if (highlightedEntry.isDefined) Some(highlightedEntry.get.getFormId) else None)
+            controller.defaultAttributeCopying(newEntity.get, Some(attributeTuples))
             (Some(newAttribute), displayStartingRowNumber)
           }
           else (highlightedEntry, attributeRowsStartingIndexIn)

src/main/scala/org/onemodel/controller/MainMenu.scala
@@ -34,7 +34,7 @@ class MainMenu(val ui: TextUI, val db: PostgreSQLDatabase, val controller: Contr
           val answer = response.get
           // None means user hit ESC (or 0, though not shown) to get out
           answer match {
-            case 1 => controller.showInEntityMenuThenMainMenu(controller.askForInfoAndCreateEntity())
+            case 1 => controller.showInEntityMenuThenMainMenu(controller.askForClassInfoAndNameAndCreateEntity())
             case 2 =>
               val selection: Option[IdWrapper] = controller.chooseOrCreateObject(None, None, None, Controller.ENTITY_TYPE)
               if (selection.isDefined) {
@@ -73,7 +73,7 @@ class MainMenu(val ui: TextUI, val db: PostgreSQLDatabase, val controller: Contr
           val answer = response.get
           answer match {
             case 1 =>
-              controller.showInEntityMenuThenMainMenu(controller.askForInfoAndCreateEntity())
+              controller.showInEntityMenuThenMainMenu(controller.askForClassInfoAndNameAndCreateEntity())
             case 2 =>
               controller.showInEntityMenuThenMainMenu(controller.askForNameAndWriteEntity(Controller.RELATION_TYPE_TYPE))
             case 3 =>

src/main/scala/org/onemodel/controller/OtherEntityMenu.scala
@@ -82,35 +82,11 @@ class OtherEntityMenu (val ui: TextUI, val db: PostgreSQLDatabase, val controlle
           otherEntityMenu(entityIn, attributeRowsStartingIndexIn, relationSourceEntityIn, containingRelationToEntityIn, containingGroupIn,
                           classDefiningEntityIdIn, attributeTuplesIn)
         } else if (answer == 3) {
-          val attributesToSuggestCopying: ArrayBuffer[Attribute] = {
-            // This determines which attributes from the template entity (or "pattern" or class-defining entity) are not found on this entity, so they can
-            // be added if the user wishes.
-            val attributesToSuggestCopying_workingCopy: ArrayBuffer[Attribute] = new ArrayBuffer()
-            if (classDefiningEntityIdIn.isDefined) {
-              // ("cde" in name means "classDefiningEntity")
-              val (cde_attributeTuples: Array[(Long, Attribute)], _) = db.getSortedAttributes(classDefiningEntityIdIn.get)
-              for (cde_attributeTuple <- cde_attributeTuples) {
-                var attributeTypeFoundOnEntity = false
-                val cde_attribute = cde_attributeTuple._2
-                for (attributeTuple <- attributeTuplesIn) {
-                  if (!attributeTypeFoundOnEntity) {
-                    val cde_typeId: Long = cde_attribute.getAttrTypeId
-                    val typeId = attributeTuple._2.getAttrTypeId
-                    if (cde_typeId == typeId) {
-                      attributeTypeFoundOnEntity = true
-                    }
-                  }
-                }
-                if (!attributeTypeFoundOnEntity) {
-                  attributesToSuggestCopying_workingCopy.append(cde_attribute)
-                }
-              }
-            }
-            attributesToSuggestCopying_workingCopy
-          }
+          val templateAttributesToCopy: ArrayBuffer[Attribute] = controller.getMissingAttributes(classDefiningEntityIdIn, attributeTuplesIn)
           val editAnswer = ui.askWhich(Some(Array[String]{controller.entityMenuLeadingText(entityIn)}),
-                                       Array("Edit entity name", "Change its class",
-                                             if (attributesToSuggestCopying.nonEmpty) "Add/edit class-defined fields" else "(stub)"))
+                                       Array("Edit entity name",
+                                             "Change its class",
+                                             if (templateAttributesToCopy.nonEmpty) "Add/edit missing class-defined fields" else "(stub)"))
           if (editAnswer.isDefined) {
             if (editAnswer.get == 1) {
               val editedEntity: Option[Entity] = controller.editEntityName(entityIn)
@@ -120,25 +96,15 @@ class OtherEntityMenu (val ui: TextUI, val db: PostgreSQLDatabase, val controlle
               val classId: Option[Long] = controller.askForClass()
               if (classId.isDefined) {
                 db.updateEntitysClass(entityIn.getId, classId)
+
+                // Idea here: when changing the class of an entity, we *could* controller.defaultAttributeCopying (or prompt as elsewhere) to
+                // set up the attributes, but the need is unclear, and user can now do that manually from the menus if needed.  Code in future
+                // should also be able to use default values from the class-defining entity, as another fallback.
               }
               otherEntityMenu(entityIn, attributeRowsStartingIndexIn, relationSourceEntityIn, containingRelationToEntityIn,
                               containingGroupIn, classDefiningEntityIdIn, attributeTuplesIn)
-            } else if (editAnswer.get == 3 && attributesToSuggestCopying.nonEmpty) {
-              var userWantsToContinue = true
-              for (attribute:Attribute <- attributesToSuggestCopying) {
-                if (userWantsToContinue) {
-                  ui.displayText("Prompting for " + PostgreSQLDatabase.getAttributeFormName(attribute.getFormId) + " \"" +
-                                 attribute.getDisplayString(0, None, None, simplify = true) + " \", as suggested by the archetype entity:",
-                                 waitForKeystroke = true)
-                  val attr: Option[Attribute] = controller.addAttribute(entityIn, attributeRowsStartingIndexIn,
-                                                                        PostgreSQLDatabase.getAttributeFormId(attribute.getClass.getSimpleName),
-                                                                        Some(attribute.getAttrTypeId))
-                  if (attr.isEmpty) {
-                    // Like a break statement: could be replaced with a functional idiom (see link to stackoverflow somewhere in the code).
-                    userWantsToContinue = false
-                  }
-                }
-              }
+            } else if (editAnswer.get == 3 && templateAttributesToCopy.nonEmpty) {
+              controller.copyAndEditAttributes(entityIn, templateAttributesToCopy)
             }
           }
         } else if (answer == 4) {

src/main/scala/org/onemodel/controller/QuickGroupMenu.scala
@@ -328,11 +328,13 @@ class QuickGroupMenu(override val ui: TextUI, override val db: PostgreSQLDatabas
             val ans: Option[Entity] = controller.askForNameAndWriteEntity(Controller.ENTITY_TYPE, inLeadingText = Some("NAME THE ENTITY:"),
                                                                inClassId = groupIn.getClassId)
             if (ans.isDefined) {
-              val newEntityId: Long = ans.get.getId
+              val newEntity = ans.get
+              val newEntityId: Long = newEntity.getId
               db.addEntityToGroup(groupIn.getId, newEntityId)
               val displayStartingRowNumber: Int = placeEntryInPosition(groupIn.getId, groupIn.getSize, 0, forwardNotBackIn = true,
                                                                        startingDisplayRowIndexIn, newEntityId,
                                                                        highlightedIndexInObjList, Some(highlightedObjId), objectsToDisplay.size, -1, Some(-1))
+              controller.defaultAttributeCopying(newEntity)
               (Some(new Entity(db, newEntityId)), displayStartingRowNumber)
             }
             else (Some(highlightedEntry), startingDisplayRowIndexIn)
@@ -372,7 +374,11 @@ class QuickGroupMenu(override val ui: TextUI, override val db: PostgreSQLDatabas
                                                                                   inClassId = groupIn.getClassId)
                     if (ans.isDefined) {
                       val newEntityId: Long = ans.get.getId
+                      val newEntity: Entity = ans.get
                       db.addEntityToGroup(targetGroupId, newEntityId)
+
+                      controller.defaultAttributeCopying(newEntity)
+
                       val newRtg: RelationToGroup = new RelationToGroup(db, rtgId, highlightedEntry.getId, relTypeId, targetGroupId)
                       quickGroupMenu(new Group(db, targetGroupId), 0, Some(newRtg), None, None, containingEntityIn = Some(highlightedEntry))
                     }

src/main/scala/org/onemodel/database/PostgreSQLDatabase.scala
@@ -31,7 +31,7 @@ import scala.util.Sorting
   */
 object PostgreSQLDatabase {
   // should these be more consistently upper-case? What is the scala style for constants?  similarly in other classes.
-  val CURRENT_DB_VERSION = 1
+  val CURRENT_DB_VERSION = 2
   val dbNamePrefix = "om_"
   val MIXED_CLASSES_EXCEPTION = "All the entities in a group should be of the same class."
   // so named to make it unlikely to collide by name with anything else:
@@ -343,7 +343,10 @@ class PostgreSQLDatabase(username: String, var password: String) {
       dbAction("create table Class (" +
                "id bigint DEFAULT nextval('ClassKeySequence') PRIMARY KEY, " +
                "name varchar(" + PostgreSQLDatabase.classNameLength + ") NOT NULL, " +
+               // In other words, template, aka class-defining entity:
                "defining_entity_id bigint UNIQUE NOT NULL, " +
+               // this means whether the user wants the program to create all the attributes by default, using the defining_entity's attrs as a template:
+               "create_default_attributes boolean, " +
                "CONSTRAINT valid_related_to_entity_id FOREIGN KEY (defining_entity_id) REFERENCES entity (id) " +
                ") ")
       dbAction("alter table entity add CONSTRAINT valid_related_to_class_id FOREIGN KEY (class_id) REFERENCES class (id)")
@@ -700,6 +703,9 @@ class PostgreSQLDatabase(username: String, var password: String) {
     if (dbVersion == 0) {
       upgradeDbFrom0to1()
     }
+    if (dbVersion == 1) {
+      upgradeDbFrom1to2()
+    }
     /* NOTE FOR FUTURE METHODS LIKE upgradeDbFrom0to1: methods like this should be designed carefully and very well-tested:
        0) make & test periodic backups of your live data to be safe!
        1) Consider designing it to be idempotent: so multiple runs on a production db (if by some mistake) will no harm (or at least will err out safely).
@@ -763,7 +769,20 @@ class PostgreSQLDatabase(username: String, var password: String) {
                "FOR EACH ROW EXECUTE PROCEDURE attribute_sorting_cleanup()")
       dbAction("CREATE TRIGGER rtg_attribute_sorting_cleanup BEFORE DELETE ON RelationToGroup " +
                "FOR EACH ROW EXECUTE PROCEDURE attribute_sorting_cleanup()")
+
       dbAction("UPDATE om_db_version SET (version) = (1)")
+    }
+    catch {
+      case e: Exception => throw rollbackWithCatch(e)
+    }
+    commitTrans()
+  }
+  private def upgradeDbFrom1to2() = {
+    beginTrans()
+    try {
+      dbAction("ALTER TABLE class ADD COLUMN create_default_attributes boolean")
+
+      dbAction("UPDATE om_db_version SET (version) = (2)")
     }
     catch {
       case e: Exception => throw rollbackWithCatch(e)
@@ -2841,15 +2860,16 @@ class PostgreSQLDatabase(username: String, var password: String) {
 
 
   def getClasses(inStartingObjectIndex: Long, inMaxVals: Option[Long] = None): java.util.ArrayList[EntityClass] = {
-    val sql: String = "SELECT id, name, defining_entity_id from class order by id limit " +
+    val sql: String = "SELECT id, name, defining_entity_id, create_default_attributes from class order by id limit " +
                       checkIfShouldBeAllResults(inMaxVals) + " offset " + inStartingObjectIndex
-    val earlyResults = dbQuery(sql, "Long,String,Long")
+    val earlyResults = dbQuery(sql, "Long,String,Long,Boolean")
     val finalResults = new java.util.ArrayList[EntityClass]
     // idea: should the remainder of this method be moved to EntityClass, so the persistence layer doesn't know anything about the Model? (helps avoid circular
     // dependencies; is a cleaner design; see similar comment in getEntitiesGeneric.)
     for (result <- earlyResults) {
-      // None of these values should be of "None" type, so not checking for that. If they are it's a bug:
-      finalResults.add(new EntityClass(this, result(0).get.asInstanceOf[Long], result(1).get.asInstanceOf[String], result(2).get.asInstanceOf[Long]))
+      // Only one of these values should be of "None" type, so not checking the others for that. If they are it's a bug:
+      finalResults.add(new EntityClass(this, result(0).get.asInstanceOf[Long], result(1).get.asInstanceOf[String], result(2).get.asInstanceOf[Long],
+                                       if (result(3).isEmpty) None else Some(result(3).get.asInstanceOf[Boolean])))
     }
     require(finalResults.size == earlyResults.size)
     finalResults
@@ -2963,24 +2983,42 @@ class PostgreSQLDatabase(username: String, var password: String) {
     sql.toString()
   }
 
-  def getEntityData(inID: Long): Array[Option[Any]] = {
-    dbQueryWrapperForOneRow("SELECT name, class_id, insertion_date, public from Entity where id=" + inID, "String,Long,Long,Boolean")
+  def getEntityData(idIn: Long): Array[Option[Any]] = {
+    dbQueryWrapperForOneRow("SELECT name, class_id, insertion_date, public from Entity where id=" + idIn, "String,Long,Long,Boolean")
   }
 
-  def getEntityName(inID: Long): Option[String] = {
-    val name: Option[Any] = getEntityData(inID)(0)
+  def getEntityName(idIn: Long): Option[String] = {
+    val name: Option[Any] = getEntityData(idIn)(0)
     if (name.isEmpty) None
     else name.asInstanceOf[Option[String]]
   }
 
-  def getClassData(inID: Long): Array[Option[Any]] = {
-    dbQueryWrapperForOneRow("SELECT name, defining_entity_id from class where id=" + inID, "String,Long")
+  def getClassData(idIn: Long): Array[Option[Any]] = {
+    dbQueryWrapperForOneRow("SELECT name, defining_entity_id, create_default_attributes from class where id=" + idIn, "String,Long,Boolean")
   }
 
-  def getClassName(inID: Long): Option[String] = {
-    val name: Option[Any] = getClassData(inID)(0)
+  def getClassName(idIn: Long): Option[String] = {
+    val name: Option[Any] = getClassData(idIn)(0)
     if (name.isEmpty) None
     else name.asInstanceOf[Option[String]]
+  }
+
+  /**
+   * @return the create_default_attributes boolean value from a given class.
+   */
+  def getClassCreateDefaultAttributes(classIdIn: Long): Option[Boolean] = {
+    val value: Option[Any]= getClassData(classIdIn)(2)
+    if (value.isEmpty) None
+    else value.asInstanceOf[Option[Boolean]]
+  }
+
+  /**
+   * @return the create_default_attributes boolean value from a given class.
+   */
+  def updateClassCreateDefaultAttributes(classIdIn: Long, value: Option[Boolean]) {
+    dbAction("update class set (create_default_attributes) = (" +
+             (if (value.isEmpty) "NULL" else if (value.get) "true" else "false") +
+             ") where id=" + classIdIn)
   }
 
   def getTextEditorCommand: String = {

commit 90282b396542414f6bc856b6d197f14d1c24e890

Fix bug caused in commit 8ea87730e which allowed ESC from y/n questions even when the program wanted the user to always explicitly answer.

src/main/scala/org/onemodel/TextUI.scala
@@ -210,7 +210,7 @@ class TextUI(args: Array[String] = Array[String](), val inIn: Option[InputStream
     controller.start()
   }
 
-  /* Returns the string entered (None if the user just wants out of this question or whatever, unless useDefaultIfBlankedIn is true).
+  /* Returns the string entered (None if the user just wants out of this question or whatever, unless escKeySkipsCriteriaCheck is false).
    * The parameter "criteria"'s Option is a function which takes a String (which will be the user input), which it checks for validity.
    * If the entry didn't meet the criteria, it repeats the question until it does or user gets out w/ ESC.
    * A simple way to let the user know why it didn't meet the criteria is to put them in the leading text.
@@ -219,7 +219,9 @@ class TextUI(args: Array[String] = Array[String](), val inIn: Option[InputStream
   final def askForString(leadingTextIn: Option[Array[String]],
                          criteriaIn: Option[(String) => Boolean] = None,
                          defaultValueIn: Option[String] = None,
-                         isPasswordIn: Boolean = false): Option[String] = {
+                         isPasswordIn: Boolean = false,
+                         //IF ADDING ANY PARAMETERS, be sure they are also passed along in the recursive call to askForString within this method, below!
+                         escKeySkipsCriteriaCheck: Boolean = true): Option[String] = {
     var count = 0
     val lastLineOfPrompt: String = {
       var lastLineOfPrompt = ""
@@ -229,8 +231,7 @@ class TextUI(args: Array[String] = Array[String](), val inIn: Option[InputStream
           if (count < leadingTextIn.get.length) {
             // all but the last one
             println(prompt)
-          }
-          else {
+          } else {
             //print(prompt)
             //if (inDefaultValue.isDefined && inDefaultValue.get.length() > 0) {
             //  print(" [defaults to " + inDefaultValue.get + "]")
@@ -275,15 +276,14 @@ class TextUI(args: Array[String] = Array[String](), val inIn: Option[InputStream
       def checkCriteria(line: String): Option[String] = {
         if (criteriaIn.isEmpty || criteriaIn.get(line)) {
           Some(line)
-        }
-        else {
+        } else {
           displayText("Didn't pass the criteria; please re-enter.")
           // this gets "recursive call not in tail position", until new version of jvm that allows scala2do it?
-          askForString(leadingTextIn, criteriaIn, defaultValueIn, isPasswordIn)
+          askForString(leadingTextIn, criteriaIn, defaultValueIn, isPasswordIn, escKeySkipsCriteriaCheck)
         }
       }
 
-      if (line.isEmpty) {
+      if (line.isEmpty && escKeySkipsCriteriaCheck) {
         None
       } else {
         checkCriteria(line)
@@ -510,9 +510,12 @@ class TextUI(args: Array[String] = Array[String](), val inIn: Option[InputStream
   def askYesNoQuestion(promptIn: String, defaultValueIn: Option[String] = Some("n"), allowBlankAnswer: Boolean = false): Option[Boolean] = {
     val ans = askForString(Some(Array[String](promptIn + " (y/n)")),
                            if (allowBlankAnswer) Some(isValidYesNoOrBlankAnswer) else Some(isValidYesNoAnswer),
-                           defaultValueIn)
-    if (ans.isEmpty) None
-    else if (allowBlankAnswer && ans.get.trim.isEmpty) None
+                           defaultValueIn, escKeySkipsCriteriaCheck = allowBlankAnswer)
+    if (allowBlankAnswer && (ans.isEmpty || ans.get.trim.isEmpty)) None
+    else if (!allowBlankAnswer && (ans.isEmpty || ans.get.trim.isEmpty)) {
+      throw new OmException("A blank answer was not allowed by the caller but was somehow found here; probably a programming mistake.")
+    }
+    else if (ans.isEmpty) throw new OmException("How did we get here?")
     else if (ans.get.toLowerCase.startsWith("y")) Some(true)
     else Some(false)
   }