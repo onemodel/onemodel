cf7ab2daeb3eba1ae799e3407af063fb38deed9f
Luke A. Call <luke213@onemodel.org>
2018-07-18 08:50:49 -0600

    Update expected number of "expect" tests. This was required for integration tests to pass, omitted by mistake from the last commit.

--- integration/src/test/java/org/onemodel/integration/controllers/EntityMenuIT.java
+++ integration/src/test/java/org/onemodel/integration/controllers/EntityMenuIT.java
@@ -71,17 +71,17 @@ public class EntityMenuIT {
       # a failure could be missed.  This makes sure it runs to completion.
       # (If you don't know what # to use, comment the line out, check the testrun.log that was just
       # updated, and update the #, then uncomment the line. Or just uncomment it, but don't commit that.) */
-      // IDEA (also in tracked tasks): check for the string "# of expected passes            476"
+      // IDEA (also in tracked tasks): check for the string "# of expected passes            479"
       // here (for some reason putting in that full text
-      // causes the check to fail, even when I tried a regex, vs. just the "476" which works), and check
-      // for a ~"Return code from dejagnu tests: 0" (which I think I tried but failed when it was in addition to the "476",
+      // causes the check to fail, even when I tried a regex, vs. just the "479" which works), and check
+      // for a ~"Return code from dejagnu tests: 0" (which I think I tried but failed when it was in addition to the "479",
       // and didn't want to use alone because it seemed like could possible pass even when less than the full # of tests
       // were run.  Having problems with expectit and might try its
       // docs again or a different toolkit for this purpose:
 //      result = expect.expect(contains("# of expected passes"));
       //NOTE: I did confirm that the next line will fail when necessary:
-      result = expect.expect(contains("476"));
-//      result = expect.expect(contains("# of expected passes            476"));
+      result = expect.expect(contains("479"));
+//      result = expect.expect(contains("# of expected passes            479"));
 
 
       /* ***************************
